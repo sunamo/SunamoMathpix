fa290385bab699c9b072c83d183965d7cb3114ad
Radek Jancik <mb@jepsano.net>
2022-04-09 10:02:12 +0200

    If the thoughts are absolutely tranquil the heavenly heart can be seen. The heavenly heart lies between sun and moon (i.e. between the two eyes). It is the home of the inner light.  To make light circulate is the deepest and most wonderful secret.  The light is easy to move, but difficult to fix.  If it is made to circulate long enought, then it crystallizes itself;  that is the natural spirit body... - Yen, Lu

--- SunamoMathpix.Tests/UnitTest1.cs
+++ SunamoMathpix.Tests/UnitTest1.cs
@@ -16,7 +16,7 @@ namespace SunamoMathpix.Tests
         [Test]
         public void TextTest()
         {
-            var bytes = TF.ReadAllBytes(@"d:\_Test\WpfApp1\WpfApp1\OpticalCharacterRecognitionUC\a.jpg").ToArray();
+            var bytes = TF.ReadAllBytes(@"D:\_Test\WpfApp1\WpfApp1\OpticalCharacterRecognitionUC\a.jpg").ToArray();
             var b = Convert.ToBase64String(bytes);
             b = "data:image/jpeg;base64," + b;
             MathpixHelper mathpixHelper = new MathpixHelper("sunamocz_gmail_com_966d39", "bc811dc177d29f2b0ce0", ExecutablePaths.curlFolder);
